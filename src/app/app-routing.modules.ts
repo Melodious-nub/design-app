5b8c29cf85a7ad7f6e987a24486cd6eea639d164
shawon taluckder <shawon.taluckder1@gmail.com>
2023-05-18 18:44:42 +0600

    updated with side hoverable tabs

--- src/app/app-routing.modules.ts
+++ src/app/app-routing.modules.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { InputsComponent } from './components/inputs/inputs.component';
 import { TabsComponent } from './components/tabs/tabs.component';
+import { ButtonsComponent } from './components/buttons/buttons.component';
+import { ModalsComponent } from './components/modals/modals.component';
 
 const routes: Routes = [
     { path:'', component: InputsComponent, pathMatch: 'full'},
     { path:'tabs', component: TabsComponent},
+    { path:'buttons', component: ButtonsComponent},
+    { path:'modals', component: ModalsComponent},
   ];
   
   @NgModule({
