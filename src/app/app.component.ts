8414c0a97e6cc9ba87012f0dbc4dbd35553c80ea
shawon taluckder <shawon.taluckder1@gmail.com>
2023-05-18 16:53:39 +0600

    updated with input designs only

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'design-app';
+
+  userMessage: string = '';
+  hide = true;
+
+  sendMessage() {
+    // Implement your logic to handle the sent message
+    console.log('User message:', this.userMessage);
+    // Clear the input field after sending
+    this.userMessage = '';
+  }
+
+  message: string = '';
+
+  adjustTextAreaHeight(textarea: any) {
+    if (textarea) {
+      textarea.style.height = 'auto';
+      textarea.style.height = textarea.scrollHeight + 'px';
+    }
+  }
+
+  onFileSelected(e:any) {
+    
+  }
 }
